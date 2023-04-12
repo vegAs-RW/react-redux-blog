f03fa48b052ce194d558af831f2b44861fe3c356
Jordan <jordan.pouchot2@gmail.com>
2023-04-12 16:26:21 +0200

    create store

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// REDUX
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers"
+
+const store = configureStore({
+    reducer: rootReducer,
+    devTools: true
+})
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById("root")
+);
