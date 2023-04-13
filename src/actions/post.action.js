093ddb49b4e41504a4a28f5e450f023375c5dd9e
Jordan <jordan.pouchot2@gmail.com>
2023-04-13 11:30:24 +0200

    add function for post and edit posts

--- src/actions/post.action.js
+++ src/actions/post.action.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 
 export const GET_POSTS = "GET_POSTS"
+export const ADD_POST = "ADD_POST"
+export const EDIT_POST = "EDIT_POST"
+
 
 export const getPosts = () => {
     return (dispatch) => {
@@ -10,3 +13,22 @@ export const getPosts = () => {
         })
     }
 }
+
+export const addPost = (data) => {
+    return (dispatch) => {
+        return axios.post("http://localhost:3000/posts", data)
+        .then((res) =>{
+            dispatch({ type: ADD_POST, payload: data})
+        })
+    }
+}
+
+export const editPost = (data) => {
+    return (dispatch) => {
+        return axios.put(`http://localhost:3000/posts/${data.id}`, data)
+        .then((res) =>{
+            dispatch({ type: EDIT_POST, payload: data})
+        })
+    }
+}
+
