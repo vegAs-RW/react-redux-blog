71bb8d5481037d37700d6c3c10d01558f3a85853
Jordan <jordan.pouchot2@gmail.com>
2023-04-13 11:31:37 +0200

    add case for EDIT_POST and ADD_POST

--- src/reducers/post.reducer.js
+++ src/reducers/post.reducer.js
@@ -1,4 +1,4 @@
-import { GET_POSTS } from "../actions/post.action";
+import { ADD_POST, EDIT_POST, GET_POSTS } from "../actions/post.action";
 
 const initialState = {}
 
@@ -6,6 +6,19 @@ export default function postReducer(state = initialState, action) {
    switch (action.type) {
     case GET_POSTS:
         return action.payload;
+    case ADD_POST:
+        return [action.payload, ...state];
+    case EDIT_POST:
+        return state.map((post) => {
+            if (post.id === action.payload.id) {
+                return {
+                    ...post,
+                    content: action.payload.content
+                };
+            } else {
+                return post
+            }
+        });
     default:
         return state;
    }
