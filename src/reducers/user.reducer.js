034cf94b9e12f89cac52ca3970d7422f7fa60773
Jordan <jordan.pouchot2@gmail.com>
2023-04-13 12:46:48 +0200

    add case for ADD_USER_LIKE

--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -1,4 +1,4 @@
-import { GET_USER } from "../actions/user.action";
+import { ADD_USER_LIKE, GET_USER } from "../actions/user.action";
 
 const initialState = {}
 
@@ -6,6 +6,12 @@ export default function userReducer(state = initialState, action) {
    switch (action.type) {
       case GET_USER:
          return action.payload
+      case ADD_USER_LIKE: 
+         return {
+             ...state,
+             likes: action.payload.likes
+         };
+           
       default: 
          return state
    }
