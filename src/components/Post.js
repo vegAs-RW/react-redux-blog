9f28818da29825becf17da6e4728c4f4afc50fcf
Jordan <jordan.pouchot2@gmail.com>
2023-04-13 11:34:23 +0200

    create handleEdit func

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { editPost } from "../actions/post.action";
 import Like from "./Like";
+import { isEmpty } from "./Utils";
 
 const Post = ({ post }) => {
   const [editToggle, setEditToggle] = useState(false);
+  const user = useSelector((state) => state.userReducer)
+  const [editContent, setEditContent] = useState(post.content);
+  const dispatch = useDispatch();
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+
+    const postData = {
+      title: post.title,
+      author: user.pseudo,
+      likes: post.likes,
+      id: post.id,
+      content: editContent,
+    }
+
+    dispatch(editPost(postData))
+    setEditToggle(false);
+  }
 
   return (
     <div className="post">
+      {!isEmpty(user) && user.pseudo === post.author && (
       <div className="edit-delete">
         <img
           src="./icons/edit.svg"
@@ -17,7 +39,7 @@ const Post = ({ post }) => {
           alt="delete"
         />
       </div>
-
+      )}
       <h2>{post.title}</h2>
       <img
         src="https://picsum.photos/1500/400"
@@ -26,8 +48,8 @@ const Post = ({ post }) => {
       />
 
       {editToggle ? (
-        <form>
-          <textarea autoFocus={true} defaultValue={post.content}></textarea>
+        <form onSubmit={(e) => handleEdit(e)}>
+          <textarea autoFocus={true} defaultValue={post.content} onChange={(e) => setEditContent(e.target.value)}></textarea>
           <input type="submit" value="Valider modification" />
         </form>
       ) : (
