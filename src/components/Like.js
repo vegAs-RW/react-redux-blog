203984572d18e2c9502ba15e843621815cc0cfe4
Jordan <jordan.pouchot2@gmail.com>
2023-04-13 12:47:55 +0200

    add func handleLike for increment user and post like onClick

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addPostlike } from "../actions/post.action";
+import { addUserlike } from "../actions/user.action";
 
 const Like = ({ post }) => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userReducer)
+
+  const handleLike = () => {
+    const postData = {
+      title: post.title,
+      author: post.author,
+      content: post.content,
+      id: post.id,
+      likes: post.likes + 1
+    };
+
+    const userData = {
+      pseudo: user.pseudo,
+      age: user.age,
+      id: user.id,
+      likes: user.likes + 1
+    }
+
+    dispatch(addPostlike(postData));
+    dispatch(addUserlike(userData));
+  }
+
+
   return (
     <div>
-      <img src="./icons/clap.png" className="clap" alt="clap" />
+      <img onClick={() => handleLike()} src="./icons/clap.png" className="clap" alt="clap" />
       <span>{post.likes}</span>
     </div>
   );
