e7c901ccabd1867e3493b97dde9a5e53443bc25b
Jordan <jordan.pouchot2@gmail.com>
2023-04-13 12:43:26 +0200

    add func for delete and addlike on posts

--- src/actions/post.action.js
+++ src/actions/post.action.js
@@ -3,6 +3,8 @@ import axios from "axios"
 export const GET_POSTS = "GET_POSTS"
 export const ADD_POST = "ADD_POST"
 export const EDIT_POST = "EDIT_POST"
+export const DELETE_POST = "DELETE_POST"
+export const ADD_POST_LIKE = "ADD_POST_LIKE"
 
 
 export const getPosts = () => {
@@ -32,3 +34,20 @@ export const editPost = (data) => {
     }
 }
 
+export const deletePost = (postId) => {
+    return (dispatch) => {
+        return axios.delete(`http://localhost:3000/posts/${postId}`)
+        .then((res) =>{
+            dispatch({ type: DELETE_POST, payload: postId})
+        })
+    }
+}
+
+export const addPostlike = (data) => {
+    return (dispatch) => {
+        return axios.put(`http://localhost:3000/posts/${data.id}`, data)
+        .then((res) =>{
+            dispatch({ type: ADD_POST_LIKE, payload: data})
+        })
+    }
+}
